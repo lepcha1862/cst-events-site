dfca757508b1865463606017eea4d34a3b96c075
lepcha1862 <gchalep07@gmail.com>
2026-09-17 14:38:26 +0600

    intetional break

--- build_site.py
+++ build_site.py
@@ -9,7 +9,7 @@ def load_events(path):
  
  
 def upcoming(events, today): 
-    future = [e for e in events if e["date"] >= today] 
+    future = [e for e in events if e["date"] > today] 
     return sorted(future, key=lambda e: e["date"]) 
  
  
